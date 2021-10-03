8e51c9ff9d80db5f01c568915e6b4ed344ee13b5
davezer <davezer27@gmail.com>
2021-10-03 19:23:47 -0400

    added console log and created playerName var

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running");
+
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10+10)
+console.log("Our robots name is " + playerName);
+
+// This creats a function named "fight"
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+
+// fight();
